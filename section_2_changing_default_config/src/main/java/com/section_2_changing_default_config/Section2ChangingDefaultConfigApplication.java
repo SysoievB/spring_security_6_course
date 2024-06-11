8dd3e0058f66cfbb3b00ffd7ce0b3d5331620ff4
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-06-11 09:33:40 +0200

    Added section 2

---
+++ section_2_changing_default_config/src/main/java/com/section_2_changing_default_config/Section2ChangingDefaultConfigApplication.java
@@ -0,0 +1,13 @@
+package com.section_2_changing_default_config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section2ChangingDefaultConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section2ChangingDefaultConfigApplication.class, args);
+    }
+
+}
---
+++ section_2_changing_default_config/src/test/java/com/section_2_changing_default_config/Section2ChangingDefaultConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_2_changing_default_config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section2ChangingDefaultConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
