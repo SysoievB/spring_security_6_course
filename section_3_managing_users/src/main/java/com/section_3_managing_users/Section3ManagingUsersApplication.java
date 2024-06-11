811c718409aaf2334d43276ea2eb3f347b465a2b
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-06-11 10:27:40 +0200

    Added section 2 - changing default config

---
+++ section_3_managing_users/src/main/java/com/section_3_managing_users/Section3ManagingUsersApplication.java
@@ -0,0 +1,13 @@
+package com.section_3_managing_users;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section3ManagingUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section3ManagingUsersApplication.class, args);
+    }
+
+}
