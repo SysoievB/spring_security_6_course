71f7bd2528d94547efb481bff06e0278a04bac74
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-06-12 08:42:01 +0200

    Added section 4 - password encoder

---
+++ section_4_password_encoder/src/main/java/com/section_4_password_encoder/Section4PasswordEncoderApplication.java
@@ -0,0 +1,13 @@
+package com.section_4_password_encoder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section4PasswordEncoderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section4PasswordEncoderApplication.class, args);
+    }
+
+}
---
+++ section_4_password_encoder/src/test/java/com/section_4_password_encoder/Section4PasswordEncoderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_4_password_encoder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section4PasswordEncoderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
