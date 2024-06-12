e7a8ab064f710b6bffc22d2f73834bc819bdb4e4
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-06-12 17:03:13 +0200

    Added section 6 - cors & csrf

---
+++ section_6_cors_csrf/src/main/java/com/section_6_cors_csrf/Section6CorsCsrfApplication.java
@@ -0,0 +1,13 @@
+package com.section_6_cors_csrf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section6CorsCsrfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section6CorsCsrfApplication.class, args);
+	}
+
+}
---
+++ section_6_cors_csrf/src/test/java/com/section_6_cors_csrf/Section6CorsCsrfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_6_cors_csrf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section6CorsCsrfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
