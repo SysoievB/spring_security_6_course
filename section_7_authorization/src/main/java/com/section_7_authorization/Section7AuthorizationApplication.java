f69ff744d8c8d00c3d519ed183341ac112229883
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-06-15 07:18:51 +0200

    Added section 7 - auth

---
+++ section_7_authorization/src/main/java/com/section_7_authorization/Section7AuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.section_7_authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section7AuthorizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Section7AuthorizationApplication.class, args);
+    }
+
+}
---
+++ section_7_authorization/src/test/java/com/section_7_authorization/Section7AuthorizationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_7_authorization;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section7AuthorizationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
