75a92136a142e4ad692f2fb164e69a813da52e33
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-06-16 11:43:14 +0200

    Added section 9 - jwt token

---
+++ section_9_jwt_token/src/main/java/com/section_9_jwt_token/Section9JwtTokenApplication.java
@@ -0,0 +1,13 @@
+package com.section_9_jwt_token;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section9JwtTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section9JwtTokenApplication.class, args);
+	}
+
+}
---
+++ section_9_jwt_token/src/test/java/com/section_9_jwt_token/Section9JwtTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_9_jwt_token;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section9JwtTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
