46ce89df96f1c07e74497b03c00bddb450a75f3a
SysoievB <sysoiev.bogdan.ua@gmail.com>
2024-06-17 18:36:01 +0200

    Added section 10 - method level security

---
+++ section_10_method_level_security/src/main/java/com/section_10_method_level_security/Section10MethodLevelSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.section_10_method_level_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Section10MethodLevelSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Section10MethodLevelSecurityApplication.class, args);
+	}
+
+}
---
+++ section_10_method_level_security/src/test/java/com/section_10_method_level_security/Section10MethodLevelSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.section_10_method_level_security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Section10MethodLevelSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
